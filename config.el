d1fc93b72270d44b61756fd57e017486a3cbef46
cchantep <c.chantepie@yahoo.fr>
2013-04-10 14:43:23 +0200

    Update ENSIME

--- config.el
+++ config.el
@@ -1,5 +1,4 @@
-t;; Set the debug option to enable a backtrace when a
-;; problem occurs.
+;; Set the debug option to enable a backtrace when a problem occurs.
 (setq debug-on-error t)
 
 ;; Disable tab \t character for indent
@@ -50,16 +49,6 @@ t;; Set the debug option to enable a backtrace when a
 (setenv "SHELL" shell-file-name)
 (setq explicit-sh-args '("-login" "-i"))
 
-;; JDEE
-;(load-file "~/Library/Emacs/site/cedet-1.1/common/cedet.el")
-;;(add-to-list 'load-path 
-;;             (expand-file-name "~/Library/Emacs/site/cedet-1.1/common"))
-;(add-to-list 'load-path (expand-file-name "~/Library/Emacs/site/jdee-2.4.0.1/lisp"))
-;(add-to-list 'load-path (expand-file-name "~/Library/Emacs/site/elib-1.0"))
-
-;(require 'jde)
-;;(global-set-key "\C-c\C-vg" 'jde-gen-get-set) ; get/set generator shortcut
-
 ;; JavaScript
 (require 'javascript-mode)
 
@@ -68,12 +57,12 @@ t;; Set the debug option to enable a backtrace when a
 
 ;; Scala
 (add-to-list 'load-path (expand-file-name "~/Library/Emacs/site/scala-mode2-20130406"))
-(add-to-list 'load-path (expand-file-name "~/Library/Emacs/site/ensime_2.9.2-RC1-0.9.3.RC4/elisp"))
+(add-to-list 'load-path (expand-file-name "~/Library/Emacs/site/ensime_2.10.0-0.9.8.9/elisp"))
 
 (require 'scala-mode2)
 (require 'ensime)
 
-(defconst scala-rigid-indent t)
+;(defconst scala-rigid-indent t)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
 ;; Scala - Ensime integration
